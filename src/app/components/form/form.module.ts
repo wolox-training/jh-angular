d41c9f68c0321a4691006ac6dee7f546e61d7c8a
Juan Miguel Henao Arias <juan.henao@wolox.co>
2021-05-24 16:45:06 -0500

    Changed structure of form component and applied change of signup screen

--- src/app/components/form/form.module.ts
+++ src/app/components/form/form.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormComponent } from './form.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [FormComponent],
   exports: [FormComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ]
 })
 export class FormModule { }
