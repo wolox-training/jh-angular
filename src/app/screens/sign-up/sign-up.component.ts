97599f8b5415064498caf3c1acec3578a53221e1
Juan Miguel Henao Arias <juan.henao@wolox.co>
2021-05-24 17:25:02 -0500

    Delete html repetive, using directive ng-for

--- src/app/screens/sign-up/sign-up.component.ts
+++ src/app/screens/sign-up/sign-up.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
-import { SignUp } from './interfaces/sign-up.interface';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.scss'],
-  providers: [FormBuilder]
+  styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent implements OnInit {
 
-  signUpForm: SignUp = { name: '', lastname: '', email: '', password: '', confirmPassword: '' };
+  fields = [
+    { label: 'Nombre', name: 'name', type: 'text'},
+    { label: 'Apellido', name: 'lastname', type: 'text'},
+    { label: 'Email', name: 'email', type: 'email'},
+    { label: 'Password', name: 'password', type: 'password'},
+    { label: 'Confirmación de Password', name: 'confirmPassword', type: 'password'},
+  ];
+  signUpForm: any = { name: '', lastname: '', email: '', password: '', confirmPassword: '' };
 
   ngOnInit(): void {
   }
