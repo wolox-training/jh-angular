f9a899f1377046ea0722288a211e69903b8d0906
Juan Miguel Henao Arias <juan.henao@wolox.co>
2021-06-01 15:29:45 -0500

    Add test 'do not record when fields are empty'

---
+++ src/app/helpers/mocks/sign-up.mock.ts
@@ -0,0 +1,10 @@
+import { SignUp } from "src/app/interfaces/sign-up.interface";
+
+export const SignUpMockEmpty = {
+  first_name: 'Juan Miguel',
+  last_name: '',
+  email: '',
+  password: '',
+  password_confirmation: '',
+  locale: 'en'
+}
--- src/app/screens/sign-up/sign-up.component.spec.ts
+++ src/app/screens/sign-up/sign-up.component.spec.ts
@@ -1,16 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { async, ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormModule } from 'src/app/components/form/form.module';
+import { SignUpMockEmpty } from 'src/app/helpers/mocks/sign-up.mock';
 
 import { SignUpComponent } from './sign-up.component';
 
-describe('SignUpComponent', () => {
+describe('SignUpComponent should', () => {
   let component: SignUpComponent;
   let fixture: ComponentFixture<SignUpComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SignUpComponent ]
+      declarations: [SignUpComponent],
+      imports: [HttpClientTestingModule, ReactiveFormsModule, FormModule, RouterTestingModule]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
@@ -22,4 +28,14 @@ describe('SignUpComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  fit('not record when fields are empty', fakeAsync(() => {
+    const onClickMock = spyOn(component, 'signUp').and.callThrough();
+    component.form.setValue(SignUpMockEmpty);
+    fixture.detectChanges();
+    let button = fixture.debugElement.nativeElement.querySelector('button');
+    button.click();
+    tick();
+    expect(onClickMock).not.toHaveBeenCalled();
+  }));
 });
