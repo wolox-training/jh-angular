f11f6b740e55efd0f35eeb636de38e9168ebfa67
Juan Miguel Henao Arias <juan.henao@wolox.co>
2021-06-04 00:33:58 -0500

    Created auth route and update others calls to the route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderModule } from './components/header/header.module';
-import { BooksComponent } from './screens/books/books.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    BooksComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
