7b788731303f9eb6e4a15f57b3f5d2840d3dadba
Juan Miguel Henao Arias <juan.henao@wolox.co>
2021-06-04 12:00:41 -0500

    Used header tag and moved logout function

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,10 +8,12 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 export class HeaderComponent {
 
-  @Output() logout = new EventEmitter<boolean>();
+  constructor(private router: Router) {
+  }
 
   sessionLogout(): void {
-    this.logout.emit(true);
+    localStorage.removeItem('ACCESS_TOKEN');
+    this.router.navigate(['auth/login']);
   }
 
 }
