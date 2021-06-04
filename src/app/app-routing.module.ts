7abe89960c9911332fe413c26462d8413c798b07
Juan Miguel Henao Arias <juan.henao@wolox.co>
2021-06-03 23:58:55 -0500

    Rename the auth folder how to unauth and update the calls to the path unauth
    Rename the auth folder how to unauth and update the calls the path auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,8 @@ import { BooksComponent } from './screens/books/books.component';
 const routes: Routes = [
   { path: '', redirectTo: 'auth', pathMatch: 'full' },
   {
-    path: 'auth',
-    loadChildren: () => import('./screens/auth/auth.module').then(m => m.AuthModule)
+    path: 'unauth',
+    loadChildren: () => import('./screens/unauth/unauth.module').then(m => m.UnAuthModule)
   },
   { path: 'books', component: BooksComponent },
 ];
