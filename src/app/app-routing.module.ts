52dfb2286abf41a1fe05b5add1f85f6dd8e3cff9
Juan Miguel Henao Arias <juan.henao@wolox.co>
2021-06-04 10:52:09 -0500

    Created guards for paths auth and unauth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './helpers/guards/auth.guard';
+import { UnAuthGuard } from './helpers/guards/unauth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'auth', pathMatch: 'full' },
   {
     path: 'unauth',
-    loadChildren: () => import('./screens/unauth/unauth.module').then(m => m.UnAuthModule)
+    loadChildren: () => import('./screens/unauth/unauth.module').then(m => m.UnAuthModule),
+    canActivate: [UnAuthGuard]
   },
   {
     path: 'auth',
-    loadChildren: () => import('./screens/auth/auth.module').then(m => m.AuthModule)
+    loadChildren: () => import('./screens/auth/auth.module').then(m => m.AuthModule),
+    canActivate: [AuthGuard]
   },
 ];
 
---
+++ src/app/helpers/guards/unauth.guard.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UnAuthGuard implements CanActivate {
+
+  constructor(private _userService: UserService, private _router: Router) {
+  }
+
+  canActivate(): boolean {
+    if (this._userService.loggedIn()) {
+      this._router.navigate(['auth/books']);
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+}
