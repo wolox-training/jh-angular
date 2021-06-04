b76d30410f3f366a45778acc0c799b264bf188f9
Juan Miguel Henao Arias <juan.henao@wolox.co>
2021-06-03 22:04:27 -0500

    Created books component, and import header component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BooksComponent } from './screens/books/books.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'auth', pathMatch: 'full' },
   {
     path: 'auth',
     loadChildren: () => import('./screens/auth/auth.module').then(m => m.AuthModule)
-  }
+  },
+  { path: 'books', component: BooksComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderModule } from './components/header/header.module';
+import { BooksComponent } from './screens/books/books.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    BooksComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HeaderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
