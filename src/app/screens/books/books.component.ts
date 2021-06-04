e5ced16b3d84a84b007d80c96343eb3a88918a0a
Juan Miguel Henao Arias <juan.henao@wolox.co>
2021-06-04 12:00:41 -0500

    Used header tag and moved logout function

--- src/app/screens/books/books.component.ts
+++ src/app/screens/books/books.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-books',
@@ -7,12 +6,5 @@ import { Router } from '@angular/router';
   styleUrls: ['./books.component.scss']
 })
 export class BooksComponent {
-
-  constructor(private router: Router) { }
-
-  logout() {
-    localStorage.removeItem('ACCESS_TOKEN');
-    this.router.navigate(['auth/login']);
-  }
 
 }
