9a1165a1b585f4025e7719ee59a006b993669cc5
Juan Miguel Henao Arias <juan.henao@wolox.co>
2021-06-06 02:28:11 -0500

    Show books in book-list component and added intercepter providers

--- src/app/screens/auth/auth.module.ts
+++ src/app/screens/auth/auth.module.ts
@@ -3,17 +3,31 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './auth.component';
+import { BookListComponent } from './book-list/book-list.component';
 import { HeaderModule } from 'src/app/components/header/header.module';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { TokenInterceptorService } from 'src/app/services/token-interceptor.service';
+import { BooksService } from 'src/app/services/books.service';
+import { BookModule } from 'src/app/components/book/book.module';
 
 
 @NgModule({
   declarations: [
-    AuthComponent
+    AuthComponent,
+    BookListComponent
   ],
   imports: [
     CommonModule,
     AuthRoutingModule,
-    HeaderModule
-  ]
+    HeaderModule,
+    HttpClientModule,
+    BookModule
+  ],
+  providers: [BooksService,
+  {
+    provide: HTTP_INTERCEPTORS,
+    useClass: TokenInterceptorService,
+    multi: true
+  }]
 })
 export class AuthModule { }
