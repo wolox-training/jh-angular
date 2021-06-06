6b16d32fba4ce1a533e95d65b950ee411f83ddce
Juan Miguel Henao Arias <juan.henao@wolox.co>
2021-06-06 02:08:24 -0500

    Created book component

---
+++ src/app/components/book/book.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BookComponent } from './book.component';
+
+
+
+@NgModule({
+  declarations: [
+    BookComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [BookComponent]
+})
+export class BookModule { }
