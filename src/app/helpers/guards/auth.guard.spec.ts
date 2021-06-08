db1859999c635bdb0a8ae4fa5371b44f7991baa0
Juan Miguel Henao Arias <juan.henao@wolox.co>
2021-06-08 02:06:15 -0500

    Created tests for Auth and UnAuth guards

--- src/app/helpers/guards/auth.guard.spec.ts
+++ src/app/helpers/guards/auth.guard.spec.ts
@@ -1,22 +1,36 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { SignInComponent } from 'src/app/screens/unauth/sign-in/sign-in.component';
 import { UserService } from 'src/app/services/user.service';
 
 import { AuthGuard } from './auth.guard';
 
-fdescribe('AuthGuard', () => {
+describe('AuthGuard', () => {
   let guard: AuthGuard;
 
-  beforeEach(() => {
+  beforeEach(async () => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule, RouterTestingModule],
+      imports: [HttpClientTestingModule, RouterTestingModule.withRoutes([{ path: 'unauth/login', component: SignInComponent }])],
       providers: [UserService, AuthGuard]
-    });
-    guard = TestBed.inject(AuthGuard);
+    }).compileComponents();
   });
+
+  beforeEach(() => {
+    guard = TestBed.inject(AuthGuard);
+  })
 
   it('should be created', () => {
     expect(guard).toBeTruthy();
+  });
+
+  it('should be able to hit route when user is logged in', () => {
+    localStorage.setItem('ACCESS_TOKEN', 'Byaslkdasdnxuaksaj-asas');
+    expect(guard.canActivate()).toBeTrue();
+  });
+
+  it('not should be able to hit route when user is logged in', () => {
+    localStorage.clear();
+    expect(guard.canActivate()).toBeFalse();
   });
 });
--- src/app/helpers/guards/unauth.guard.spec.ts
+++ src/app/helpers/guards/unauth.guard.spec.ts
@@ -1,6 +1,7 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { BookListComponent } from 'src/app/screens/auth/book-list/book-list.component';
 import { UserService } from 'src/app/services/user.service';
 
 import { UnAuthGuard } from './unauth.guard';
@@ -10,7 +11,7 @@ describe('UnAuthGuard', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule, RouterTestingModule],
+      imports: [HttpClientTestingModule, RouterTestingModule.withRoutes([{ path: 'auth/books', component: BookListComponent }])],
       providers: [UserService]
     });
     guard = TestBed.inject(UnAuthGuard);
@@ -18,5 +19,15 @@ describe('UnAuthGuard', () => {
 
   it('should be created', () => {
     expect(guard).toBeTruthy();
+  });
+
+  it('should not be able to hit route unauth when user is logged in', () => {
+    localStorage.setItem('ACCESS_TOKEN', 'Byaslkdasdnxuaksaj-asas');
+    expect(guard.canActivate()).toBeFalse();
+  });
+
+  it('should be able to hit route unauth when user is not logged in', () => {
+    localStorage.clear();
+    expect(guard.canActivate()).toBeTrue();
   });
 });
