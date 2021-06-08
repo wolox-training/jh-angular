b914be110232f28ae74de5d301a6b689682328d9
Juan Miguel Henao Arias <juan.henao@wolox.co>
2021-06-08 10:52:29 -0500

    Created test for filter event

--- src/app/screens/auth/book-list/book-list.component.spec.ts
+++ src/app/screens/auth/book-list/book-list.component.spec.ts
@@ -1,14 +1,22 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
+import { BooksFiltered, BooksMockResponse } from 'src/app/helpers/mocks/books.mock';
+import { BooksService } from 'src/app/services/books.service';
 
 import { BookListComponent } from './book-list.component';
 
-describe('BookListComponent', () => {
+fdescribe('BookListComponent', () => {
   let component: BookListComponent;
   let fixture: ComponentFixture<BookListComponent>;
+  let booksService: BooksService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ BookListComponent ]
+      declarations: [ BookListComponent ],
+      imports: [HttpClientTestingModule, RouterTestingModule],
+      providers: [BooksService]
     })
     .compileComponents();
   });
@@ -16,10 +24,40 @@ describe('BookListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BookListComponent);
     component = fixture.componentInstance;
+    booksService = TestBed.get(BooksService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should get all books from service', () => {
+    spyOn(booksService, 'getBooks').and.returnValue(of(BooksMockResponse));
+    component.ngOnInit();
+
+    booksService.getBooks().subscribe(res => {
+      expect(res).toEqual(BooksMockResponse);
+    });
+  });
+
+  it('should filter books with input', () => {
+    component.books = BooksMockResponse.page;
+    const input: HTMLInputElement = fixture.debugElement.nativeElement.querySelector('#search');
+    input.value = 'the';
+    fixture.detectChanges();
+    input.dispatchEvent(new Event('input'));
+
+    expect(component.booksFiltered).toEqual(BooksFiltered);
+  });
+
+  it('should filter books with input empty', () => {
+    component.books = BooksMockResponse.page;
+    const input: HTMLInputElement = fixture.debugElement.nativeElement.querySelector('#search');
+    input.value = '';
+    fixture.detectChanges();
+    input.dispatchEvent(new Event('input'));
+
+    expect(component.booksFiltered).toEqual(BooksMockResponse.page);
   });
 });
